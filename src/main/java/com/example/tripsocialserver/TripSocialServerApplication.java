31180f7515ddfbee01c0e7b81cad1491695aed86
Malik <willis.malik.r@gmail.com>
2023-06-15 14:21:45 -0500

    first commit

---
+++ src/main/java/com/example/tripsocialserver/TripSocialServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.tripsocialserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripSocialServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripSocialServerApplication.class, args);
+    }
+
+}
