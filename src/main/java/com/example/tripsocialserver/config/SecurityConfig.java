db570f1b2e4b9776757beba93411ffa226f4d6cc
Malik <willis.malik.r@gmail.com>
2023-06-15 14:49:29 -0500

    repositories, services, models, and controller setup

--- src/main/java/com/example/tripsocialserver/config/SecurityConfig.java
+++ src/main/java/com/example/tripsocialserver/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.tripsocialserver.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,6 +12,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
