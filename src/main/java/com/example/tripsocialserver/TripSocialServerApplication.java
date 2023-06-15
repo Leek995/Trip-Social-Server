d0fa1f2a9d05f4ff2562969ac205b7ef8c63125a
Malik <willis.malik.r@gmail.com>
2023-06-15 15:13:41 -0500

    db connect verified by commnad line runner, able to store user

--- src/main/java/com/example/tripsocialserver/TripSocialServerApplication.java
+++ src/main/java/com/example/tripsocialserver/TripSocialServerApplication.java
@@ -1,13 +1,25 @@
 package com.example.tripsocialserver;
 
+import com.example.tripsocialserver.models.User;
+import com.example.tripsocialserver.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TripSocialServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TripSocialServerApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(UserRepository users){
+        return args -> {
+            users.save(new User("user", "password", "ROLE_USER"));
+            users.save(new User("admin", "password", "ROLE_ADMIN"));
+        };
     }
 
 }
